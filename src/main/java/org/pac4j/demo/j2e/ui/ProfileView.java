15ebd67e5b037c7d9e43f48c5cf37a11af9f2288
Phillip Ross <phillip.w.g.ross@gmail.com>
2017-05-19 17:26:21 -0400

    - Consolidated configuration into single class. - logout filter for local logouts now configured programmatically - Added logout action for JSF which called logout method of Pac4j profile manager

--- src/main/java/org/pac4j/demo/j2e/ui/ProfileView.java
+++ src/main/java/org/pac4j/demo/j2e/ui/ProfileView.java
@@ -1,4 +1,4 @@
-package org.pac4j.demo.j2e;
+package org.pac4j.demo.j2e.ui;
 
 
 import org.pac4j.core.context.WebContext;
@@ -14,6 +14,13 @@ import javax.inject.Named;
 import java.util.List;
 
 
+/**
+ * Managed bean which exposes the Pac4J profile manager.
+ *
+ * JSF views such as facelets can reference this to view the contents of profiles.
+ *
+ * @author Phillip Ross
+ */
 @Named
 @RequestScoped
 public class ProfileView {
@@ -21,9 +28,11 @@ public class ProfileView {
     /** The static logger instance. */
     private static final Logger logger = LoggerFactory.getLogger(ProfileView.class);
 
+    /** The Pac4j web context. */
     @Inject @Pac4j
     private WebContext webContext;
 
+    /** The Pac4j profile manager. */
     @Inject @Pac4j
     private ProfileManager profileManager;
 
@@ -32,11 +41,17 @@ public class ProfileView {
     }
 
 
+    /**
+     * Gets the profiles contained in the profile manager.
+     *
+     * @return a list of Pac4j profiles
+     */
     public List getProfiles() {
         return profileManager.getAll(true);
     }
 
 
+    /** Simply prints some debugging information post-construction. */
     @PostConstruct
     public void init() {
         logger.debug("webContext is null? {}", (webContext == null));
