d2350e15da080646bb12e60a3c3cbba7b3fd650b
leleuj <leleuj@gmail.com>
2017-08-13 08:23:01 +0200

    update to pac4j v2.1

--- src/main/java/org/pac4j/demo/j2e/SecurityConfig.java
+++ src/main/java/org/pac4j/demo/j2e/SecurityConfig.java
@@ -92,8 +92,8 @@ public class SecurityConfig {
         final IndirectBasicAuthClient indirectBasicAuthClient = new IndirectBasicAuthClient(new SimpleTestUsernamePasswordAuthenticator());
 
         // CAS
-        //final CasConfiguration configuration = new CasConfiguration("https://casserverpac4j.herokuapp.com/login");
-        final CasConfiguration configuration = new CasConfiguration("http://localhost:8888/cas/login");
+        final CasConfiguration configuration = new CasConfiguration("https://casserverpac4j.herokuapp.com/login");
+        //final CasConfiguration configuration = new CasConfiguration("http://localhost:8888/cas/login");
         final CasProxyReceptor casProxy = new CasProxyReceptor();
         configuration.setProxyReceptor(casProxy);
         final CasClient casClient = new CasClient(configuration);
